c06a6da799363b3f6a34217756b5f90912790a3c
Sagar-Lekhwani <sgrlekhwani@gmail.com>
2024-02-26 22:28:00 +0530

    refokus building initial state

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Work from './components/Work'
+import Stripes from './components/Stripes'
+
+
+const App = () => {
+  return (
+    <div className='h-screen w-full bg-zinc-900 text-white'>
+      <Navbar/>
+      <Work />
+      <Stripes />
+    </div>
+  )
+}
+
+export default App
